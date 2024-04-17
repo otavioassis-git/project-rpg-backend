9ddb7defc26116f732e8b5e17b4559e7b4817192
otavioassis-git <otavioassis2013@gmail.com>
2024-04-16 22:28:24 -0300

    wip

--- src/modules/image/controllers/image.controller.ts
+++ src/modules/image/controllers/image.controller.ts
@@ -1,14 +1,14 @@
 import { AuthGuard } from 'src/guards/auth/auth.guard';
 import { ImageService } from './../services/image.service';
-import { Body, Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Req, UseGuards } from '@nestjs/common';
 
 @UseGuards(AuthGuard)
 @Controller('images')
 export class ImageController {
   constructor(private imageService: ImageService) {}
 
-  @Get()
-  findAll() {
-    return this.imageService.findAllImages();
+  @Get('comunity')
+  findAll(@Req() request) {
+    return this.imageService.findComunityImages(request);
   }
 }
--- src/modules/image/services/image.service.ts
+++ src/modules/image/services/image.service.ts
@@ -1,6 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Image } from 'src/models/image.model';
+import { User } from 'src/models/user.model';
 
 @Injectable()
 export class ImageService {
@@ -9,7 +10,33 @@ export class ImageService {
     private imageModel: typeof Image,
   ) {}
 
-  async findAllImages() {
-    return await this.imageModel.findAll({ attributes: ['id', 'value'] });
+  async findComunityImages(req) {
+    const images = await this.imageModel
+      .findAll({
+        attributes: ['id', 'value'],
+        include: {
+          model: User,
+          attributes: ['username'],
+          through: { attributes: [] },
+        },
+      })
+      .then((images) => {
+        let indexes: number[] = [];
+        for (let [idx, image] of images.entries()) {
+          const index = image.dataValues.users.findIndex(
+            (user) => user.username == req.user.username,
+          );
+          if (index >= 0) {
+            indexes.push(idx);
+          }
+          delete image.dataValues['users'];
+        }
+        for (let index of indexes) {
+          images.splice(index, 1);
+        }
+        return images;
+      });
+
+    return images;
   }
 }
